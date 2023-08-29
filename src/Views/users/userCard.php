7ff375cc61414e71060c831cbae53beef4d837d1
todano <90718070+todano@users.noreply.github.com>
2023-08-29 21:05:46 +0300

    Update userCard.php
    Fix image issue.

--- src/Views/users/userCard.php
+++ src/Views/users/userCard.php
@@ -1,6 +1,10 @@
 <div class="col-4">
     <div class="card" style="width: 18rem;">
-        <img src="/<?= $user['imgPath'] ;?>" class="card-img-top" alt="...">
+        <?php if($user['default_img']):?>
+          <td><img src="/src/img/users/<?= $user['id']?>/<?= $user['default_img']?>" class="card-img-top" alt="..."></td>
+        <?php else:?>
+          <td><img src="/src/img/users/default.jpg" class="card-img-top" alt="..."></td>
+        <?php endif ;?>
         <div class="card-body bg-info">
             <h5 class="card-title"><?= $user['name'].' '.$user['last_name'] ?></h5>
             <h5 class="card-text"> <?= $user['username'] ?></h5>
